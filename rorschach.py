73a1fc4bc27f36aee3dba8a92498bd2652c10796
jaaneshwar07 <95646331+jaaneshwar07@users.noreply.github.com>
2022-09-02 17:15:13 +0530

    Add files via upload

---
+++ rorschach.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class Rorschach(WilloughbyEngine):
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        return bool(service_threshold_date < datetime.now().date() or self.engine_should_be_serviced())
